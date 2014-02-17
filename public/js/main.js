ddb95f5de1ce9a580bb143a2ce6b0dfd992cbe8f
David Hazinski <dhazinski@gmail.com>
2014-02-16 23:34:06 -0500

    vehicle count in infowindow

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,28 @@ function forEach(list, callback) {
     }
 }
 
+function createInfoWindow(agency, callback) {
+    var agency_id = agency.agency_id,
+        content;
+
+    if (agency.url) {
+        content = '<a target="_blank" href="' + agency.url + '">' + agency.long_name + '</a>';
+    } else {
+        content = '<div>' + agency.long_name + '</div>';
+    }
+
+    $.getJSON('/api/vehicles?agencies=' + agency_id, function(res) {
+        var vehicles = res.data[agency_id] || [],
+            numVehicles = vehicles.length;
+
+        content += '<div>' + numVehicles + ' vehicles</div>';
+
+        callback(new google.maps.InfoWindow({
+            content: content
+        }));
+    });
+}
+
 var mapOptions = {
     zoom: 4,
     center: new google.maps.LatLng(35, -95)
@@ -18,15 +40,7 @@ $.getJSON('/api/agencies', function(res) {
     forEach(res.data, function(agency) {
 
         var position = new google.maps.LatLng(agency.position.lat, agency.position.lng),
-            marker,
-            infowindow,
-            content;
-
-        if (agency.url) {
-            content = '<a target="_blank" href="' + agency.url + '">' + agency.long_name + '</a>';
-        } else {
-            content = '<div>' + agency.long_name + '</div>';
-        }
+            marker;
 
         marker = new google.maps.Marker({
             position: position,
@@ -34,16 +48,13 @@ $.getJSON('/api/agencies', function(res) {
             title: agency.long_name
         });
 
-        infowindow = new google.maps.InfoWindow({
-            content: content
-        });
-
         google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map,marker);
-        });
-
-        google.maps.event.addListener(map, 'click', function() {
-            infowindow.close();
+            createInfoWindow(agency, function(infowindow) {
+                infowindow.open(map,marker);
+                google.maps.event.addListener(map, 'click', function() {
+                    infowindow.close();
+                });
+            });
         });
     });
-})
+});
